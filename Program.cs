1f98a4add66f73a783f1ce7e1653af6b9ae92b59
imseigen <seigenjane@gmail.com>
2025-03-17 00:04:30 +0700

    Menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("103022330128"); 
+        data.PrintData();
+    }
+}
